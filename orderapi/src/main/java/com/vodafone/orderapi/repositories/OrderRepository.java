aa2aec71aed9669e1c52d1871fbaf6dcd7ad7521
Parameswari Bala <parameswaribala@gmail.com>
2025-01-14 10:48:36 +0530

    updated

--- orderapi/src/main/java/com/vodafone/orderapi/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vodafone.orderapi.repositories;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
--- orderapi/src/main/java/com/vodafone/orderapi/services/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.vodafone.orderapi.services;
-
-import org.springframework.core.annotation.Order;
-
-public interface OrderDao {
-    Order addOrder(Order order);
-}
