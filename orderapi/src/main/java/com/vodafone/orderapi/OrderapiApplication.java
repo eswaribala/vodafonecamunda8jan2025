501f6b50e40580f8607635d0fbc46b88ca222e5e
Parameswari Bala <parameswaribala@gmail.com>
2025-01-14 11:55:31 +0530

    updated

--- orderapi/src/main/java/com/vodafone/orderapi/OrderapiApplication.java
+++ orderapi/src/main/java/com/vodafone/orderapi/OrderapiApplication.java
@@ -1,9 +1,11 @@
 package com.vodafone.orderapi;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Deployment(resources ={ ("classpath*:/processes/**/*.bpmn"),("classpath*:/decisions/**/*.dmn")})
 public class OrderapiApplication {
 
     public static void main(String[] args) {
