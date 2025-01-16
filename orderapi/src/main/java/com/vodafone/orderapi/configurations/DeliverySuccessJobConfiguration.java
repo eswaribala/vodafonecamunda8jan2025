94b47a5a7a9a7ae98bd188a1353525bd6fdd1e1b
Parameswari Bala <parameswaribala@gmail.com>
2025-01-16 13:15:19 +0530

    updated

--- orderapi/src/main/java/com/vodafone/orderapi/configurations/DeliverySuccessJobConfiguration.java
+++ orderapi/src/main/java/com/vodafone/orderapi/configurations/DeliverySuccessJobConfiguration.java
@@ -1,11 +1,8 @@
 package com.vodafone.orderapi.configurations;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vodafone.orderapi.models.Order;
-import com.vodafone.orderapi.models.OrderStatus;
 import com.vodafone.orderapi.services.OrderDao;
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
@@ -13,22 +10,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @Slf4j
-public class DeliveryIssueJobConfiguration {
-
+public class DeliverySuccessJobConfiguration {
     @Autowired
-    private OrderDao orderDao;
-    @JobWorker(type = "deliveryissue", autoComplete = false)
-    public Map<String,Boolean> deliveryIssue(final JobClient jobClient, final ActivatedJob activatedJob) throws JsonProcessingException {
+    private ZeebeClient zeebeClient;
+
+
+    @JobWorker(type = "deliverysuccessupdate", autoComplete = false)
+    public Map<String,Boolean> deliverySuccess(final JobClient jobClient, final ActivatedJob activatedJob) throws JsonProcessingException {
 
 
          Map<String,Boolean> orderMap=new HashMap<>();
-         orderMap.put("deliveryStatus",false);
+         orderMap.put("deliveryStatus",true);
+        zeebeClient.newPublishMessageCommand()
+                .messageName("Message_Delivery_Success_Message")
+                .correlationKey("2002")
 
+                // .variables(subProcess)
+                .timeToLive(Duration.ofMinutes(3))
+                .send()
+
+                .exceptionally((throwable)->{
+                    throw new RuntimeException("Job not found");
+                });
         jobClient.newCompleteCommand(activatedJob.getKey())
                 .variables(orderMap)
                 .send()
