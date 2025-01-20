512523d7af2a9880de2e62d908c2d0610c920318
Parameswari Bala <parameswaribala@gmail.com>
2025-01-20 11:16:04 +0530

    updated

---
+++ deliveryapi/src/main/java/com/vodafone/deliveryapi/configurations/ProcessConstant.java
@@ -0,0 +1,7 @@
+package com.vodafone.deliveryapi.configurations;
+
+public class ProcessConstant {
+
+    public static final String Order_BPMN_Process_Constant="Process_Order";
+    public static final String DB_BPMN_Process_Constant="Process_Postgres";
+}
