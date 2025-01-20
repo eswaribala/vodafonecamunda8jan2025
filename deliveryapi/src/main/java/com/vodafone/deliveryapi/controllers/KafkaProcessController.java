c9c079546bdae5f048233f877cc9ee56662dba3c
Parameswari Bala <parameswaribala@gmail.com>
2025-01-20 12:39:07 +0530

    updated

--- deliveryapi/src/main/java/com/vodafone/deliveryapi/controllers/KafkaProcessController.java
+++ deliveryapi/src/main/java/com/vodafone/deliveryapi/controllers/KafkaProcessController.java
@@ -3,12 +3,16 @@ package com.vodafone.deliveryapi.controllers;
 import com.vodafone.deliveryapi.configurations.ProcessConstant;
 import com.vodafone.deliveryapi.dtos.GenericResponse;
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("kafkaconsumers")
@@ -21,12 +25,17 @@ public class KafkaProcessController {
     @GetMapping("/v1.0")
     ResponseEntity<?> startDBProcess(){
 
-        this.zeebeClient
+        Map<String,String> map=new HashMap<>();
+         map.put("kafkaOrderId","57757");
+         this.zeebeClient
                 .newCreateInstanceCommand()
                 .bpmnProcessId(ProcessConstant.KAFKA_CONSUMER_BPMN_Process_Constant)
                 .latestVersion()
-                //.variables(variables)
+                .variables(map)
                 .send();
+
+        //System.out.println(processInstanceEvent.getProcessInstanceKey());
+
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Instance created for"+ProcessConstant.KAFKA_CONSUMER_BPMN_Process_Constant);
 
